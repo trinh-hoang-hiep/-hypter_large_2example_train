4b834101239407fd732eaa65b225ec2dacd78f15
trinh-hoang-hiep <dinhmanhhoang96@gmail.com>
2022-07-09 16:00:20 +0700

    cap nhat lai  file run_shh

--- scripts/zest_grouped_bart_large_from_trained.sh
+++ scripts/zest_grouped_bart_large_from_trained.sh
@@ -1,34 +1,32 @@
 # 538 tasks in total
-
+    
 CUDA_VISIBLE_DEVICES=0 \
 python cli_grouped.py \
- #--checkpoint ./out/bart-large-zest55-reproduce/last-model.pt \ #############################3
  --learning_rate 1e-5 \
- --seed 55 \
+ --seed 1 \
  --dataset zest_grouped \
  --inner_bsz 16 \
  --adapter_dim 32 \
  --do_train \
- --do_predict \
  --freeze_embeds \
  --total_steps 2020 \
  --warmup_steps 120 \
  --max_grad_norm 0.1 \
  --weight_decay 0.01 \
  --model facebook/bart-large \
- --output_dir ./out/bart-large-zest-from-trained  \ #######################################
- --train_file ./data/zest_train.jsonl \ #############################
+ --output_dir ./out/bart-large-zest-from-trained  \
+ --train_file ./data/zest_train.jsonl \
  --predict_file data/zest_dev.jsonl \
- --train_batch_size 2 \
+ --train_batch_size 1 \
  --gradient_accumulation_steps 1 \
  --predict_batch_size 16 \
- --max_input_length 256 \
+ --max_input_length 128 \
  --max_output_length 64 \
  --eval_period 1076 \
  --num_train_epochs 120;
- 
- #--train_batch_size 1 \
-#  CUDA_VISIBLE_DEVICES=2 \
+ # --do_predict \
+ # --max_input_length 32 \
+  # --checkpoint ./out/bart-large-zest55-reproduce/last-model.pt \
 # cd ~/zest/bin
 # python evaluate-zest.py -p ~/zs/out/bart-large-zest-from-trained/predictions.txt -d ~/zest/data/dev.jsonl -o ~/zs/out/bart-large-zest-from-trained/results.json
 
